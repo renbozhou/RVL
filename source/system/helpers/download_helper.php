0d21bd247e8215b5bc0801d97868205df0222a0d
luzhenwei <549943405@qq.com>
2016-10-24 11:01:57 +0800

    ci 3.1.0 字体库更新和system/helpers文件库的更新

--- source/system/helpers/download_helper.php
+++ source/system/helpers/download_helper.php
@@ -1,19 +1,41 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
 /**
  * CodeIgniter
  *
- * An open source application development framework for PHP 5.1.6 or newer
+ * An open source application development framework for PHP
  *
- * @package		CodeIgniter
- * @author		ExpressionEngine Dev Team
- * @copyright	Copyright (c) 2008 - 2011, EllisLab, Inc.
- * @license		http://codeigniter.com/user_guide/license.html
- * @link		http://codeigniter.com
- * @since		Version 1.0
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014 - 2016, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
+ * @copyright	Copyright (c) 2014 - 2016, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	https://codeigniter.com
+ * @since	Version 1.0.0
  * @filesource
  */
-
-// ------------------------------------------------------------------------
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
  * CodeIgniter Download Helpers
@@ -21,239 +43,116 @@
  * @package		CodeIgniter
  * @subpackage	Helpers
  * @category	Helpers
- * @author		ExpressionEngine Dev Team
- * @link		http://codeigniter.com/user_guide/helpers/download_helper.html
+ * @author		EllisLab Dev Team
+ * @link		https://codeigniter.com/user_guide/helpers/download_helper.html
  */
 
 // ------------------------------------------------------------------------
 
-/**
- * Force Download
- *
- * Generates headers that force a download to happen
- *
- * @access	public
- * @param	string	filename
- * @param	mixed	the data to be downloaded
- * @return	void
- */
 if ( ! function_exists('force_download'))
 {
-function force_download($filename = '', $data = false, $enable_partial = true, $speedlimit = 0)
-    {
-        if ($filename == '')
-        {
-            return FALSE;
-        }
-        
-        if($data === false && !file_exists($filename)) {        
-            echo $filename."File not found.";
-            return FALSE;
-        }
-
-        // Try to determine if the filename includes a file extension.
-        // We need it in order to set the MIME type
-        if (FALSE === strpos($filename, '.'))
-        {
-            return FALSE;
-        }
-    
-        // Grab the file extension
-        $x = explode('.', $filename);
-        $extension = end($x);
-
-        // Load the mime types
-        @include(APPPATH.'config/mimes'.EXT);
-    
-        // Set a default mime if we can't find it
-        if ( ! isset($mimes[$extension]))
-        {
-            if (ereg('Opera(/| )([0-9].[0-9]{1,2})', $_SERVER['HTTP_USER_AGENT']))
-                $UserBrowser = "Opera";
-            elseif (ereg('MSIE ([0-9].[0-9]{1,2})', $_SERVER['HTTP_USER_AGENT']))
-                $UserBrowser = "IE";
-            else
-                $UserBrowser = '';
-            
-            $mime = ($UserBrowser == 'IE' || $UserBrowser == 'Opera') ? 'application/octetstream' : 'application/octet-stream';
-        }
-        else
-        {
-            $mime = (is_array($mimes[$extension])) ? $mimes[$extension][0] : $mimes[$extension];
-        }
-        
-        $size = $data === false ? filesize($filename) : strlen($data);
-        
-        if($data === false)
-        {
-            $info = pathinfo($filename);
-            $name = $info['basename'];
-        }
-        else
-        {
-            $name = $filename;
-        }
-        
-        // Clean data in cache if exists
-        @ob_end_clean();
-        
-        // Check for partial download
-        if(isset($_SERVER['HTTP_RANGE']) && $enable_partial)
-        {
-            list($a, $range) = explode("=", $_SERVER['HTTP_RANGE']);
-            list($fbyte, $lbyte) = explode("-", $range);
-            
-            if(!$lbyte)
-                $lbyte = $size - 1;
-            
-            $new_length = $lbyte - $fbyte;
-            
-            header("HTTP/1.1 206 Partial Content", true);
-            header("Content-Length: $new_length", true);
-            header("Content-Range: bytes $fbyte-$lbyte/$size", true);
-        }
-        else
-        {
-            header("Content-Length: " . $size);
-        }
-        
-        // Common headers
-        header('Content-Type: ' . $mime, true);
-        header('Content-Disposition: attachment; filename="' . $name . '"', true);
-        header("Expires: Mon, 26 Jul 1997 05:00:00 GMT", true);
-        header('Accept-Ranges: bytes', true);
-        header("Cache-control: private", true);
-        header('Pragma: private', true);
-        
-        // Open file
-        if($data === false) {
-            $file = fopen($filename, 'r');
-            
-            if(!$file)
-                return FALSE;
-        }
-        
-        // Cut data for partial download
-        if(isset($_SERVER['HTTP_RANGE']) && $enable_partial)
-            if($data === false)
-                fseek($file, $range);
-            else
-                $data = substr($data, $range);
-        
-        // Disable script time limit
-        @set_time_limit(0);
-        
-        // Check for speed limit or file optimize
-        if($speedlimit > 0 || $data === false)
-        {
-            if($data === false)
-            {
-                $chunksize = $speedlimit > 0 ? $speedlimit * 1024 : 512 * 1024;
-            
-                while(!feof($file) and (connection_status() == 0))
-                {
-                    $buffer = fread($file, $chunksize);
-                    echo $buffer;
-                    flush();
-                    
-                    if($speedlimit > 0)
-                        sleep(1);
-                }
-                
-                fclose($file);
-            }
-            else
-            {
-                $index = 0;
-                $speedlimit *= 1024; //convert to kb
-                
-                while($index < $size and (connection_status() == 0))
-                {
-                    $left = $size - $index;
-                    $buffersize = min($left, $speedlimit);
-                    
-                    $buffer = substr($data, $index, $buffersize);
-                    $index += $buffersize;
-                    
-                    echo $buffer;
-                    flush();
-                    sleep(1);
-                }
-            }
-        }
-        else
-        {
-            echo $data;
-        }
-    } 
-
-
-	/*
-    
-    function force_download($filename = '', $data = '')
+	/**
+	 * Force Download
+	 *
+	 * Generates headers that force a download to happen
+	 *
+	 * @param	string	filename
+	 * @param	mixed	the data to be downloaded
+	 * @param	bool	whether to try and send the actual file MIME type
+	 * @return	void
+	 */
+	function force_download($filename = '', $data = '', $set_mime = FALSE)
 	{
-		if ($filename == '' OR $data == '')
+		if ($filename === '' OR $data === '')
 		{
-			return FALSE;
+			return;
+		}
+		elseif ($data === NULL)
+		{
+			if ( ! @is_file($filename) OR ($filesize = @filesize($filename)) === FALSE)
+			{
+				return;
+			}
+
+			$filepath = $filename;
+			$filename = explode('/', str_replace(DIRECTORY_SEPARATOR, '/', $filename));
+			$filename = end($filename);
+		}
+		else
+		{
+			$filesize = strlen($data);
 		}
 
-		// Try to determine if the filename includes a file extension.
-		// We need it in order to set the MIME type
-		if (FALSE === strpos($filename, '.'))
-		{
-			return FALSE;
-		}
+		// Set the default MIME type to send
+		$mime = 'application/octet-stream';
 
-		// Grab the file extension
 		$x = explode('.', $filename);
 		$extension = end($x);
 
-		// Load the mime types
-		if (defined('ENVIRONMENT') AND is_file(APPPATH.'config/'.ENVIRONMENT.'/mimes.php'))
+		if ($set_mime === TRUE)
 		{
-			include(APPPATH.'config/'.ENVIRONMENT.'/mimes.php');
-		}
-		elseif (is_file(APPPATH.'config/mimes.php'))
-		{
-			include(APPPATH.'config/mimes.php');
+			if (count($x) === 1 OR $extension === '')
+			{
+				/* If we're going to detect the MIME type,
+				 * we'll need a file extension.
+				 */
+				return;
+			}
+
+			// Load the mime types
+			$mimes =& get_mimes();
+
+			// Only change the default MIME if we can find one
+			if (isset($mimes[$extension]))
+			{
+				$mime = is_array($mimes[$extension]) ? $mimes[$extension][0] : $mimes[$extension];
+			}
 		}
 
-		// Set a default mime if we can't find it
-		if ( ! isset($mimes[$extension]))
+		/* It was reported that browsers on Android 2.1 (and possibly older as well)
+		 * need to have the filename extension upper-cased in order to be able to
+		 * download it.
+		 *
+		 * Reference: http://digiblog.de/2011/04/19/android-and-the-download-file-headers/
+		 */
+		if (count($x) !== 1 && isset($_SERVER['HTTP_USER_AGENT']) && preg_match('/Android\s(1|2\.[01])/', $_SERVER['HTTP_USER_AGENT']))
 		{
-			$mime = 'application/octet-stream';
+			$x[count($x) - 1] = strtoupper($extension);
+			$filename = implode('.', $x);
 		}
-		else
+
+		if ($data === NULL && ($fp = @fopen($filepath, 'rb')) === FALSE)
 		{
-			$mime = (is_array($mimes[$extension])) ? $mimes[$extension][0] : $mimes[$extension];
+			return;
+		}
+
+		// Clean output buffer
+		if (ob_get_level() !== 0 && @ob_end_clean() === FALSE)
+		{
+			@ob_clean();
 		}
 
 		// Generate the server headers
-		if (strpos($_SERVER['HTTP_USER_AGENT'], "MSIE") !== FALSE)
+		header('Content-Type: '.$mime);
+		header('Content-Disposition: attachment; filename="'.$filename.'"');
+		header('Expires: 0');
+		header('Content-Transfer-Encoding: binary');
+		header('Content-Length: '.$filesize);
+		header('Cache-Control: private, no-transform, no-store, must-revalidate');
+
+		// If we have raw data - just dump it
+		if ($data !== NULL)
 		{
-			header('Content-Type: "'.$mime.'"');
-			header('Content-Disposition: attachment; filename="'.$filename.'"');
-			header('Expires: 0');
-			header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-			header("Content-Transfer-Encoding: binary");
-			header('Pragma: public');
-			header("Content-Length: ".strlen($data));
-		}
-		else
-		{
-			header('Content-Type: "'.$mime.'"');
-			header('Content-Disposition: attachment; filename="'.$filename.'"');
-			header("Content-Transfer-Encoding: binary");
-			header('Expires: 0');
-			header('Pragma: no-cache');
-			header("Content-Length: ".strlen($data));
+			exit($data);
 		}
 
-		exit($data);
+		// Flush 1MB chunks of data
+		while ( ! feof($fp) && ($data = fread($fp, 1048576)) !== FALSE)
+		{
+			echo $data;
+		}
+
+		fclose($fp);
+		exit;
 	}
-    */
 }
-
-
-/* End of file download_helper.php */
-/* Location: ./system/helpers/download_helper.php */
