c9e2e164a6d72c7f80c9b64031e93e60a2d8fa64
luzhenwei <549943405@qq.com>
2016-10-26 13:02:00 +0800

    application/config/xxx-dist.php为环境配置文件, 到测试环境和真实环境需要把 xxx-dist.php文件改成 xxx.php

--- source/application/config/config-dist.php
+++ source/application/config/config-dist.php
@@ -23,8 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-//$config['base_url']	= '';
-$config['base_url'] = 'http://ap.rvl.org:101';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
@@ -371,7 +370,7 @@ $config['encryption_key'] = 'jlkahdl1245436hlkgf534l#$^!@djfgj';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = 'D:/devtools/wamp64/tmp';
+$config['sess_save_path'] = '/tmp';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
